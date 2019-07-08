ab5c2976e76d61e03c60eeb7ab017202e0b7210b
ills5771 <ilyayelizarov1@gmail.com>
2019-07-08 14:09:52 -0700

    adds basic hash table func

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -15,7 +15,9 @@ class Pair:
 # '''
 class BasicHashTable:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.count = 0
+        self.storage = [None] * capacity
 
 
 # '''
